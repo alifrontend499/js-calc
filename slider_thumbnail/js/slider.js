a6c8d333827fb044cdac3815fc4c1d827de161fe
alifrontend499 <aliofficial499@gmail.com>
2017-12-10 22:38:39 +0530

    css changes and other repsponsive changes

--- slider_thumbnail/js/slider.js
+++ slider_thumbnail/js/slider.js
@@ -1,16 +1,40 @@
 $(function () {
-    
-    var active = false;
+
     $('.c-small-img').click(function (e) {
         var smImgSrc = $(this).attr('src');
         $(this).parent().addClass('active').siblings().removeClass('active');
-        if(active){
-            return;
-        } 
-        active = true;
-        $('.c-big-img').attr('src',smImgSrc,function(){
-            active = false;
-        });
+        $('.c-big-img').fadeOut(300).attr('src', smImgSrc).fadeIn(300);
     });
-    
+
+    $('.c-btn').click(function () {
+        if ($(this).hasClass('c-left')) {
+//           disable btns when prev not found
+            if ($('.c-slider-left li.active').prev().length == 0) {
+                $(this).addClass('no_more');
+                return;
+            }
+//           remove Class when prev not found
+            $(this).removeClass('no_more');
+            $('.c-right').removeClass('no_more');
+
+            var prevImgSrc = $('.c-slider-left li.active').prev('li').find('img').attr('src');
+            $('.c-slider-left li.active').removeClass('active').prev('li').addClass('active');
+            $('.c-big-img').fadeOut(300).attr('src', prevImgSrc).fadeIn(300);
+//           alert('prev');
+        } else {
+//           disable btns when next not found
+            if ($('.c-slider-left li.active').next().length == 0) {
+                $(this).addClass('no_more');
+                return;
+            }
+//           remove Class when next not found           
+            $(this).removeClass('no_more');
+            $('.c-left').removeClass('no_more');
+
+            var nextImgSrc = $('.c-slider-left li.active').next('li').find('img').attr('src');
+            $('.c-slider-left li.active').removeClass('active').next('li').addClass('active');
+            $('.c-big-img').fadeOut(300).attr('src', prevImgSrc).fadeIn(300);
+//           alert('next');
+        }
+    });
 });
