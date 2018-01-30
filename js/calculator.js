e7e207a26226cf9b8eab95ce6c31669bd302afd0
alifrontend499 <aliofficial499@gmail.com>
2018-01-30 20:59:29 +0530

    all changes saved

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,33 @@
 
 window.onload = function(e){
-    var main_input = document.querySelectorAll('label');
-    
-    Array.from(main_input).forEach(function(name){
-       name.addEventListener('click',function(){
-          alert('name'); 
-       });
-    });
+    var calc_input, btn, inputData;
+    calc_input = document.getElementById('calc_input');
+    btn = document.querySelectorAll('label');
+
+    // loop through labels
+    for (var i = 0; i < btn.length; i++) {
+      btn[i].addEventListener('click', actionPerform);
+    }
+
+    function actionPerform(e) {
+      e.preventDefault();
+      inputData = this.firstElementChild.value;
+      calc_input.value += inputData;
+    }
+
+    function plus () {
+
+    }
+    function minus () {
+
+    }
+    function multiply () {
+
+    }
+    function devide () {
+
+    }
+    function equal () {
+
+    }
 }
